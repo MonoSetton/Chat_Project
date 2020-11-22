3aa464bf3cbc5e959be721535bf6fc86cf4ba9b9
MonoSetton <maks4onet.pl@gmail.com>
2020-11-22 17:58:11 +0100

    Add logout/register and login login and register having troubles

--- app/src/main/java/com/example/javaproject/RegisterActivity.java
+++ app/src/main/java/com/example/javaproject/RegisterActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -41,9 +44,29 @@ public class RegisterActivity extends AppCompatActivity {
         registerBtn = findViewById(R.id.buttonRegister);
 
     //Firebase Auth
-    //auth = FirebaseAuth.getInstance();
         auth = FirebaseAuth.getInstance();
 
+    //  Adding Event Listener to button Register
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username_text = userET.getText().toString();
+                String email_text = emailET.getText().toString();
+                String pass_text = passET.getText().toString();
+
+                if (TextUtils.isEmpty(username_text) || TextUtils.isEmpty(email_text) || TextUtils.isEmpty((pass_text)))
+                {
+                    Toast.makeText(RegisterActivity.this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    RegisterNow(username_text, email_text, pass_text);
+                }
+
+
+
+            }
+        });
 
     }
 
@@ -73,18 +96,21 @@ public class RegisterActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<Void> task) {
 
                                     if (task.isSuccessful()){
-                                        Intent i = new Intent();
+                                        Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(i);
+                                        finish();
+
+
+
                                     }
-
-
-
 
                                 }
                             });
 
-
-
-
+                        } else
+                        {
+                            Toast.makeText(RegisterActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
                         }
 
 
