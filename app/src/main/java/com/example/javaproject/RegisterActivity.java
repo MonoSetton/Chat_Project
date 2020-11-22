008bc5f27d5f7cb10f9fc662322870a7ea169290
MonoSetton <maks4onet.pl@gmail.com>
2020-11-22 12:44:51 +0100

    Backup

---
+++ app/src/main/java/com/example/javaproject/RegisterActivity.java
@@ -0,0 +1,101 @@
+package com.example.javaproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    //Widgets
+    EditText userET, passET, emailET;
+    Button registerBtn;
+
+    //Firebase
+    FirebaseAuth auth;
+    DatabaseReference myRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initializing Widgets:
+        userET = findViewById(R.id.userEditText);
+        passET = findViewById(R.id.passEditText);
+        emailET = findViewById(R.id.emailEditText);
+        registerBtn = findViewById(R.id.buttonRegister);
+
+    //Firebase Auth
+    //auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+
+    }
+
+    private void RegisterNow(final String username, String email, String password)
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            myRef = FirebaseDatabase.getInstance().getReference("MyUsers").child(userid);
+
+                            // HashMaps
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
+
+
+                            // Opening the Main Activity after Success Registration
+                            myRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()){
+                                        Intent i = new Intent();
+                                    }
+
+
+
+
+                                }
+                            });
+
+
+
+
+                        }
+
+
+
+                    }
+                });
+    }
+
+
+
+
+
+
+}
